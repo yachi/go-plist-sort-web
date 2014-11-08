853290432cf682d055d1b6ae8dfe6c55e162d1bd
Yachi Lo <yaachi@gmail.com>
2014-11-09 00:23:45 +0800

    parse plist and encode again

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,8 @@
 package controllers
 
 import "github.com/revel/revel"
+import "howett.net/plist"
+import "bytes"
 
 type App struct {
 	*revel.Controller
@@ -10,6 +12,18 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Plist(plist string) revel.Result {
-	return c.Render(plist)
+func (c App) Plist(plist_input string) revel.Result {
+	// string to bytes and unmarshal
+	plist_bytes := []byte(plist_input)
+	var plist_dict map[string]map[string]interface{}
+	plist.Unmarshal(plist_bytes, &plist_dict)
+
+	// encode to plist xml string
+	var plist_buf bytes.Buffer
+	encoder := plist.NewEncoder(&plist_buf)
+	encoder.Indent("  ")
+	encoder.Encode(plist_dict)
+	plist_result := plist_buf.String()
+
+	return c.Render(plist_result)
 }
